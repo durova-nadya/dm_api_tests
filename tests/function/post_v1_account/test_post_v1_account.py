efb1de2965ac7f3e63c46991b127deee013c17e8
durova-nadya <joyhope@yandex.ru>
2025-08-07 10:37:29 +0300

    фикс имя клиента

--- tests/function/post_v1_account/test_post_v1_account.py
+++ tests/function/post_v1_account/test_post_v1_account.py
@@ -12,7 +12,7 @@ def test_post_v1_account():
     account_api = AccountApi(host=host)
     login_api = LoginApi(host=host)
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
-    login = 'snowman5'
+    login = 'snowman6'
     email = f'{login}@mail.ru'
     password = '777777777'
     json_data = {
