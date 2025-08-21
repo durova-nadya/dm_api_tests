a684b6942e864cc363175dd112c0ea824e681602
durova-nadya <joyhope@yandex.ru>
2025-08-21 22:50:54 +0300

    Добавлен негативный тест создания юзера

--- tests/function/post_v1_account/test_post_v1_account.py
+++ tests/function/post_v1_account/test_post_v1_account.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
+import pytest
 from hamcrest import assert_that, has_property, starts_with, all_of, instance_of, has_properties, equal_to
+
+from checkers.http_checkers import check_status_code_http
 
 
 def test_post_v1_account(account_helper, prepare_user):
@@ -32,5 +35,18 @@ def test_post_v1_account(account_helper, prepare_user):
         )
     )
     print(response)
+
+
+@pytest.mark.parametrize('login, password, email', [
+    ('X', '123445678', 'AnnaKurt@mail.ru'),
+    ('AnnaKurt', '1', 'AnnaKurt@mail.ru'),
+    ('AnnaK', '12345678', 'AnnaK2mail.ru'),
+])
+def test_post_v1_account_negative(account_helper, login, password, email):
+    with check_status_code_http(400, "Validation failed"):
+        account_helper.register_new_user(login=login, password=password, email=email)
+
+
+
 
 
